fd8c5660f3d14ac2a9aa4af515de745e30d335d1
detunu <74103149+detunu@users.noreply.github.com>
2021-05-29 14:07:57 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+// My birthday is 2007 April 2
+let My_lucky_number = 4 + 2 + 2 + 7
+if (My_lucky_number >= 10) {
+    My_lucky_number = 1 + 5
+} else {
+    basic.showString("" + (My_lucky_number))
+}
+basic.showString("" + (My_lucky_number))
